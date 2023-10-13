79a9e28910135ab2ea179f6012060ebf9822d268
ord1naryman <ord1naryman.dmitry@gmail.com>
2023-10-13 16:19:47 +0300

    add lab1

--- lab1/functions.cpp
+++ lab1/functions.cpp
@@ -1,11 +1,17 @@
 #include "functions.h"
 
 
-void makeFirstOne(vector<vector<double>>& table, int row) {
+bool makeFirstOne(vector<vector<double>>& table, int row) {
 	double first = table[row][row];
+    if (first == 0) {
+        cout << "NO ZERO IN DIAGONAL!!! / no solutions\n";
+        printMatrix(table);
+        return false;
+    }
 	for (int i = row; i < table[row].size(); i++) {
 		table[row][i] /= first;
 	}
+    return true;
 }
 
 void makeZerosInCol(vector<vector<double>> &table, int col) {
@@ -17,17 +23,30 @@ void makeZerosInCol(vector<vector<double>> &table, int col) {
 	}
 }
 
-void makeDiagonal(vector<vector<double>> &table) {
+bool makeDiagonal(vector<vector<double>> &table) {
 	double len = min(table.size(), table[0].size());
 	for (int i = 0; i < len; i++) {
-		makeFirstOne(table, i);
+		if (!makeFirstOne(table, i)) {
+            return false;
+        }
 		makeZerosInCol(table, i);
-	}
+    }
+    return true;
 }
 
-double* calculate(vector<vector<double>>& table) {
-	double* ans = new double[table.size() + 1];
-	ans[table.size()] = 1;
+void printMatrix(vector<vector<double>> &vector) {
+    for (int i = 0; i < vector.size(); i++) {
+        for (int j = 0; j < vector[0].size(); j++) {
+            cout << vector[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+vector<double> calculate(const vector<vector<double>>& table) {
+//	double* ans = new double[table.size()];
+    vector<double> ans(table.size());
+    ans[table.size() - 1] = 1;
 	for (int i = table.size() - 1; i >= 0; i--) {
 		ans[i] = table[i][table[0].size() - 1];
 		for (int j = table[0].size() - 2; j > i; j--) { 
@@ -35,5 +54,36 @@ double* calculate(vector<vector<double>>& table) {
 		}
 	}
 	return ans;
+}
 
+vector<double> solve(const vector<vector<double>>& table) {
+    vector<vector<double>> copy(table);
+    if (!makeDiagonal(copy)) {
+        return {};
+    }
+    return calculate(copy);
+}
+
+vector<double> residual_vec(const vector<vector<double>>& table, const vector<double>& ans) {
+    vector<double> resid;
+    for (int i = 0; i < table.size(); i++) {
+        double my_res = 0;
+        for (int j = 0; j < table[i].size() - 1; j++) {
+            my_res += table[i][j] * ans[j];
+        }
+        resid.push_back(my_res - table[i][table[i].size() - 1]);
+    }
+    return resid;
+}
+
+vector<double> my_ans(const vector<vector<double>>& table, const vector<double>& ans) {
+    vector<double> my_ans;
+    for (int i = 0; i < table.size(); i++) {
+        double my_res = 0;
+        for (int j = 0; j < table[i].size() - 1; j++) {
+            my_res += table[i][j] * ans[j];
+        }
+        my_ans.push_back(my_res);
+    }
+    return my_ans;
 }
--- lab1/main.cpp
+++ lab1/main.cpp
@@ -6,20 +6,54 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	vector<vector<double>> table = vector<vector<double>>();
-	for (int i = 0; i < 3; i++) {
+	vector<vector<double>> table{};
+
+    int rowCount = stoi(argv[1]);
+    int colCount = stoi(argv[2]);
+
+	for (int i = 0; i < rowCount; i++) {
 		table.push_back(vector<double>());
 	}
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 4; j++) {
-			table[i].push_back(stof(argv[i * 4 + j + 1]));
+	for (int i = 0; i < rowCount; i++) {
+		for (int j = 0; j < colCount; j++) {
+			table[i].push_back(stof(argv[i * colCount + j + 3]));
 		}
 	}
-	makeDiagonal(table);
-	double* ans = calculate(table);
 
-	for (int i = 0; i < 3; i++) {
+    cout << "starting computation\n";
+	vector<double> ans = solve(table);
+    vector<double> resid = residual_vec(table, ans);
+	for (int i = 0; i < rowCount; i++) {
 		cout << ans[i] << " ";
 	}
+
+    cout << "\n resid vec\n";
+    for (double i : resid) {
+        cout << i << " ";
+    }
+
+    vector<double> my_ans_vec = my_ans(table, ans);
+
+    vector<vector<double>> help_sys(table);
+    for (int i = 0; i < table.size(); i++) {
+        help_sys[i][table[i].size() - 1] = my_ans_vec[i];
+    }
+
+    vector<double> help_ans = solve(help_sys);
+
+    double max_diff = INT_MIN, max_ans = INT_MIN;
+    for (int i = 0; i < ans.size(); i++) {
+        max_ans = max(max_ans, ans[i]);
+        max_diff = max(max_diff, abs(ans[i] - help_ans[i]));
+    }
+
+    if (max_ans == 0) {
+        cout << "NO!";
+        exit(1);
+    }
+    double relative_error = max_diff / max_ans;
+
+    cout << "\nrelative error: " << relative_error;
+
 	return 0;
 }
