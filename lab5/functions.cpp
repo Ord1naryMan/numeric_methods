d7593939d34dc696c02757a9a64aca362f452451
ord1naryman <ord1naryman.dmitry@gmail.com>
2023-11-09 20:28:46 +0300

    add links to solved integrals on some websites

--- lab5/functions.cpp
+++ lab5/functions.cpp
@@ -1,6 +1,6 @@
 #include "functions.h"
 
 
-double function_v5(const double& x) { return sqrt(pow(sin(x), 2) / (1 + pow(x, 3))); }
+double function_v5(const double& x) { return pow(sin(x), 2) / sqrt((1 + pow(x, 3))); }
 
 double function_v30(const double& x, const double& y) { return 1 / pow(x + y, 2); }
--- lab5/main.cpp
+++ lab5/main.cpp
@@ -8,6 +8,9 @@
 using namespace std;
 
 int main() {
+
+    //https://www.kontrolnaya-rabota.ru/s/integral/opredelennyij/?top=1.234&function=sin%5E2%28x%29%2F%281+%2B+x%5E3%29%5E%281%2F2%29&X=x&bottom=0
+
     cout << "Trapezoid method:" << endl;
     cout << "Epsilon = 1e-4: "
         << trapezoid::solve_integral(function_v5, 0.0, 1.234, 1e-4)
@@ -27,6 +30,9 @@ int main() {
         << endl;
 
     cout << endl;
+
+
+    //https://calculator-online.org/doubleintegral?base_pod=integral.double&xtop=4&xbottom=3&x=x&ytop=2&ybottom=1&function_2=1%2F(x%2By)%5E2&y=y&function_1=4*x*y%20%2B%203*x%5E2*y%5E2&y_1=y&x_1=x&cond_1=y%20%3D%20x&cond_2=y%20%3D%202*x%20%2B%209&cond_3=x%5E2%20%3D%20y&cond_4=x%20%3E%200&cond_5=y%20%3E%200
 
     cout << "Cubic simpsons method (V 30):" << endl;
     cout << "Epsilon = 1e-4: "
