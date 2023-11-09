38e6d6dfadb467458689ff0f150f25947f2d0b95
ord1naryman <ord1naryman.dmitry@gmail.com>
2023-11-09 20:17:17 +0300

    add main function

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,3 +1,48 @@
+#include <iostream>
+
+#include "functions.h"
+#include "simpsons_method.h"
+#include "cubic_simpsons_method.h"
+#include "trapezoid_method.h"
+
+using namespace std;
+
 int main() {
-    return 0;
+    cout << "Trapezoid method:" << endl;
+    cout << "Epsilon = 1e-4: "
+        << trapezoid::solve_integral(function_v5, 0.0, 1.234, 1e-4)
+        << endl;
+    cout << "Epsilon = 1e-5: "
+        << trapezoid::solve_integral(function_v5, 0.0, 1.234, 1e-5)
+        << endl;
+
+    cout << endl;
+
+    cout << "Simpsons method:" << endl;
+    cout << "Epsilon = 1e-4: "
+        << simpson::solve_integral(function_v5, 0.0, 1.234, 1e-4)
+        << endl;
+    cout << "Epsilon = 1e-5: "
+        << simpson::solve_integral(function_v5, 0.0, 1.234, 1e-5)
+        << endl;
+
+    cout << endl;
+
+    cout << "Cubic simpsons method (V 30):" << endl;
+    cout << "Epsilon = 1e-4: "
+        << cubic_simpson::solve_integral(
+                function_v30, 3.0,
+                4.0, 1.0,
+                2.0, 1e-4
+            )
+        << endl;
+    cout << "Epsilon = 1e-5: "
+        << cubic_simpson::solve_integral(
+                function_v30, 3.0,
+                4.0, 1.0,
+                2.0, 1e-5
+            )
+        << endl;
+
+    cout << endl;
 }
